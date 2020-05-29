2a08d499c4c1ecf10a456d7d579df1f6057c58be
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-29 19:38:30 +0530

    import firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from  'firebase/app';
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDFygPb72R8CAFAKhlXh6q7V-SHGcmdPU0",
+    authDomain: "react-uploadimage.firebaseapp.com",
+    databaseURL: "https://react-uploadimage.firebaseio.com",
+    projectId: "react-uploadimage",
+    storageBucket: "react-uploadimage.appspot.com",
+    messagingSenderId: "204043728231",
+    appId: "1:204043728231:web:99a43419ab0c12dfbf2bc2",
+    measurementId: "G-TXPCBJH3XX"
+  };
+
+  firebase.initializeApp(config);
