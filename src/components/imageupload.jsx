dedf288d1e2b9bc7b3987e3bdea95b90ea787103
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-29 21:43:30 +0530

    Update imageupload.jsx

--- src/components/imageupload.jsx
+++ src/components/imageupload.jsx
@@ -4,11 +4,24 @@ class ImageUpload extends Component {
     constructor(props) {
         super(props) ;
             this.state = {  }
+            this.handleChange = this.handleChange.bind(this);
+        }
+        handleChange = e => {
+            if(e.target.files[0]){
+                
+            }
         }
         render() {
+            const style = {
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center'
+            };
             return (
-                <div>
-                    <input type="file" />
+                <div style={style}>
+                    <input type="file" onChange={this.handleChange}/>
                     <button>Upload</button>
                 </div>
             )
