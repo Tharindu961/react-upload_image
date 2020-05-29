91d8a262a4fc1187be2116fe1fa6fdc11a47b97a
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-29 22:00:03 +0530

    Update imageupload.jsx

--- src/components/imageupload.jsx
+++ src/components/imageupload.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import{storage} from '../firebase' ;
 
 class ImageUpload extends Component {
     constructor(props) {
         super(props) ;
-            this.state = {  }
+            this.state = { 
+                image: null,
+                url: ''
+             }
             this.handleChange = this.handleChange.bind(this);
         }
         handleChange = e => {
             if(e.target.files[0]){
-                
+                const image =  e.target.files[0];
+                this.setState(() => ({image}));
+                this.handleUpload = this.handleUpload.bind(this);
             }
         }
+        handleUpload = () => {
+            const {image} = this.state;
+            const uploadTask = storage.ref(`images/${image.name}`).put(image);
+            uploadTask.on('state_changed', progress, error, complete);
+        }
+
         render() {
             const style = {
                 height: '100vh',
@@ -22,7 +34,7 @@ class ImageUpload extends Component {
             return (
                 <div style={style}>
                     <input type="file" onChange={this.handleChange}/>
-                    <button>Upload</button>
+                    <button onClick={this.handleUpload}>Upload</button>
                 </div>
             )
         }
