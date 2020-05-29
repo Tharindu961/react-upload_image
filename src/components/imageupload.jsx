57cea279fadf5c8ba4c5656378fbe45fe277e23d
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-29 20:20:24 +0530

    Update imageupload.jsx

--- src/components/imageupload.jsx
+++ src/components/imageupload.jsx
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 class ImageUpload extends Component {
     constructor(props) {
-        super(props) {
+        super(props) ;
             this.state = {  }
         }
-    }
+        render() {
+            return (
+                <div>
+                    <input type="file" />
+                    <button>Upload</button>
+                </div>
+            )
+        }
 }
+
+export default ImageUpload;
